4e7f3baacf3feb2376163b8c31eecc95683eece8
bgr26 <borgomez026@gmail.com>
2020-04-19 16:51:20 +0200

    Modified main in testing branch

--- Test_Project/src/main.cpp
+++ Test_Project/src/main.cpp
@@ -4,6 +4,7 @@ int main(int argc, char **argv){
 	std::cout << "Test 1" << std::endl;
 	std::cout << "Test 3" << std::endl;
 	std::cout << "Test 2" << std::endl;
+	std::cout << "Test 4" << std::endl;
 	return 0;
 }
 
